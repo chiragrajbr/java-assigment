95b25255195ee22e7da1b316f62d72c5fdbc86ea
chiragrajbr <115540327+chiragrajbr@users.noreply.github.com>
2023-01-09 12:50:24 +0530

    ok

---
+++ conditional/2.java
@@ -0,0 +1,10 @@
+//check the no is multiple by 3 or not
+class ok{
+	public static void main(String args[]){
+		for(int i=0;i<=30;i++){
+			if(i%3==0){
+				System.out.println(i);
+			}
+		}
+	}
+}
---
+++ looping/21.java
@@ -0,0 +1,10 @@
+//1-10 odd no using for loop
+class ok{
+	public static void main(String args[]){
+		for(int i=1;i<=10;i++){
+			if(i%2!=0){
+				System.out.println(i);
+			}
+		}
+	}
+}
